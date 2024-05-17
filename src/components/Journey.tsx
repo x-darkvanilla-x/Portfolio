64b9d04b1f85aab0c099c2442869eb388a3c308a
Dipesh <dipesh.adelkar@gmail.com>
2024-05-17 17:22:29 +0530

    journey

--- src/components/Journey.tsx
+++ src/components/Journey.tsx
@@ -1,5 +1,5 @@
 import { AccountBalance, School, Work } from "@mui/icons-material";
-import { Avatar, Stack, Typography, useMediaQuery } from "@mui/material";
+import { Avatar, Link, Stack, Typography, useMediaQuery } from "@mui/material";
 
 function getRandomLightColor() {
   const hue = Math.floor(Math.random() * 360);
@@ -16,7 +16,7 @@ const journeydata = [
     title: "Experience",
     year: "2022-2023",
     content:
-      "Completed one month Internship in React Js with Internity Foundation",
+      "I Worked as a Freelance Graphic Designer For Aaliya Tailoring Institute Mumbra, Mumbai. Designed promotional materials and branding elements for Aaliya Tailoring Institute, enhancing their visual identity and market presence",
   },
   {
     icon: <School />,
@@ -24,7 +24,7 @@ const journeydata = [
     title: "Graduation at Sahyog College, Thane",
     year: "2022-Present",
     content:
-      "Completed one month Internship in React Js with Internity Foundation",
+      "I'm Currently Pursing Bachelors in Information Technology with an average of 9.7 CGPA",
   },
   {
     icon: <AccountBalance />,
@@ -32,15 +32,20 @@ const journeydata = [
     title: "Higher Education",
     year: "2021",
     content:
-      "I have completed my higher education from HKIS with major subjects as Physics,Chemistry & Maths with 85% merit in CBSE board.",
+      "I have completed my higher education from MatoShree College of Science with major subjects as Physics,Chemistry & Maths in HSC board.",
   },
   {
     icon: <Work />,
     color: getRandomLightColor(),
     title: "Internship",
     year: "",
-    content:
-      "Completed one month Internship in React Js with Internity Foundation",
+    content: "Completed Web Development Internships from : ",
+    links: [
+      { name: "Internpe", url: "https://internpe.in/" },
+      { name: "Codesoft", url: "https://www.codsoft.in/" },
+      { name: "OctaNet", url: "https://octanet.in/" },
+      { name: "TechnoHacks", url: "https://technohacks.co.in/" },
+    ],
   },
 ];
 
@@ -53,19 +58,62 @@ export const Journey = () => {
         <Typography variant={isMobile ? "body1" : "h6"}>MY JOURNEY</Typography>
 
         <Stack display={"flex"} flexWrap={"wrap"} direction={"row"} gap={3}>
-          {journeydata.map((item) => (
-            <Stack sx={{ padding: "30px", width: "100%", backgroundColor: "#F1F2F2"}}>
-                <Stack gap={2}>
-                  <Stack direction={isMobile? "column" : "row"} alignItems="center" gap={1.5}>
-                    <Avatar sx={{backgroundColor: item.color, padding: "5px", '& > *': { color: 'black' }}}>
-                      {item.icon}
-                    </Avatar>
-                    <Typography variant="h6" textAlign={isMobile? "center" : "left"}>{item.title}</Typography>
-                    <Typography variant="body2"  color={"gray"}>{item.year}</Typography>
-                  </Stack>
-
-                  <Typography variant={isMobile? "body2": "body1"}  textAlign={isMobile? "center" : "left"}>{item.content}</Typography>
+          {journeydata.map((item, index) => (
+            <Stack
+              key={index}
+              sx={{
+                padding: "30px",
+                width: "100%",
+                backgroundColor: "#F1F2F2",
+              }}
+            >
+              <Stack gap={2}>
+                <Stack
+                  direction={isMobile ? "column" : "row"}
+                  alignItems="center"
+                  gap={1.5}
+                >
+                  <Avatar
+                    sx={{
+                      backgroundColor: item.color,
+                      padding: "5px",
+                      "& > *": { color: "black" },
+                    }}
+                  >
+                    {item.icon}
+                  </Avatar>
+                  <Typography
+                    variant="h6"
+                    textAlign={isMobile ? "center" : "left"}
+                  >
+                    {item.title}
+                  </Typography>
+                  <Typography variant="body2" color={"gray"}>
+                    {item.year}
+                  </Typography>
                 </Stack>
+                <Typography
+                  variant={isMobile ? "body2" : "body1"}
+                  textAlign={isMobile ? "center" : "left"}
+                >
+                  {item.content}
+                </Typography>
+                {item.links && (
+                  <Stack direction={"row"} gap={1} flexWrap={"wrap"}>
+                    {item.links.map((link, linkIndex) => (
+                      <Typography
+                        variant={isMobile ? "body2" : "body1"}
+                        textAlign={isMobile ? "center" : "left"}
+                      >
+                        <Link href={link.url} target="_blank">
+                          {link.name}
+                        </Link>{" "}
+                        {linkIndex < item.links.length - 1 && "|"}
+                      </Typography>
+                    ))}
+                  </Stack>
+                )}
+              </Stack>
             </Stack>
           ))}
         </Stack>
